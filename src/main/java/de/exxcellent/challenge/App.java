3ce55b21d7f775b6bc6638f4be16a36038feabb6
René Hertling <hertling@gmail.com>
2020-11-15 13:06:41 +0100

    chore(*): Setup initial build environment to use gradle

--- src/main/java/de/exxcellent/challenge/App.java
+++ src/main/java/de/exxcellent/challenge/App.java
@@ -13,13 +13,5 @@ public final class App {
      * @param args The CLI arguments passed
      */
     public static void main(String... args) {
-
-        // Your preparation code …
-
-        String dayWithSmallestTempSpread = "Someday";     // Your day analysis function call …
-        System.out.printf("Day with smallest temperature spread : %s%n", dayWithSmallestTempSpread);
-
-        String teamWithSmallestGoalSpread = "A good team"; // Your goal analysis function call …
-        System.out.printf("Team with smallest goal spread       : %s%n", teamWithSmallestGoalSpread);
     }
 }
--- src/test/java/de/exxcellent/challenge/AppTest.java
+++ src/test/java/de/exxcellent/challenge/AppTest.java
@@ -1,31 +1,18 @@
 package de.exxcellent.challenge;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
 
 /**
  * Example JUnit 5 test case.
+ * 
  * @author Benjamin Schmid <benjamin.schmid@exxcellent.de>
  */
 class AppTest {
 
-    private String successLabel = "not successful";
-
-    @BeforeEach
-    void setUp() {
-        successLabel = "successful";
-    }
-
-    @Test
-    void aPointlessTest() {
-        assertEquals("successful", successLabel, "My expectations were not met");
-    }
-
-    @Test
-    void runFootball() {
-        App.main("--football", "football.csv");
-    }
-
+	@Test
+	void firstTest() throws Exception {
+		fail("Please implement");
+	}
 }
